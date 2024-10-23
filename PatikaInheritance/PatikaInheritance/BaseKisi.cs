e0023256a8951d19e3c05143492dda776192516e
BerkanGH <berkankeskin28@gmail.com>
2024-10-23 04:12:37 +0300

    odev

---
+++ PatikaInheritance/PatikaInheritance/BaseKisi.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaInheritance
+{
+    public class BaseKisi
+    {
+        public string ad { get; set; }
+        public string soyAd { get; set; }
+
+        public string BilgileriGöster()
+        {
+
+            return $"adı: {ad} soyadı: {soyAd}";
+
+        }
+
+    }
+    public class Ogrenci : BaseKisi
+
+    { 
+        public string ogrNo { get; set; }
+
+        public void OgrenciBilgileriGöster() 
+           
+        
+         {
+            
+            Console.WriteLine($" {BilgileriGöster()} ogrenci no: {ogrNo} "); // bilgileri gösteri kalıtım aldık.
+            
+        
+        
+         }
+
+      
+
+
+     }
+
+    public class Ogretmen : BaseKisi
+
+    {
+
+        public int maas { get; set; }
+
+        public void OgretmenBilgileriGöster()
+        {
+             
+            Console.WriteLine($"{BilgileriGöster()} maaşı: {maas} "); // bilgilerigösteri kalıtımla aldık.
+        
+        
+        }
+
+
+
+
+
+
+    }
+
+}
---
+++ PatikaInheritance/PatikaInheritance/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace PatikaInheritance {
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Ogrenci ogrenci1 = new Ogrenci();
+
+            ogrenci1.ad = "aleyna";
+            ogrenci1.soyAd = "arslan";
+
+            ogrenci1.ogrNo = "5135131230";
+
+
+            ogrenci1.OgrenciBilgileriGöster();
+
+
+
+
+
+
+            Ogretmen ogretmen1 = new Ogretmen();
+            ogretmen1.ad = "yağız";
+            ogretmen1.soyAd = "can";
+            ogretmen1.maas = 35000;
+
+            ogretmen1.OgretmenBilgileriGöster();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+        }
+
+
+}
